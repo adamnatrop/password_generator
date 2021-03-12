990b386a9b311cf84ca4bb2933327d84b002f6e0
Adam Natrop <adamnatrop@gmail.com>
2021-03-11 20:53:28 -0600

    generator working until added askUser function

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ var allSymbols = "!@#$%^&*()"
 var symbols = allSymbols.split("");
 var numbers = allNumbers.split("");
 var lowerCase = letters.split("");
+var upperCase = lowerCase.map(lower => lower.toUpperCase());  // converts lowercase letters to uppercase letters
 
-// converts lowercase letters to uppercase letters
-var upperCase = lowerCase.map(lower => lower.toUpperCase());
+
+
+//var characterCount = "";
+
+//var newPassword = "";
 
 //displays arrays in console 
 
@@ -23,16 +27,16 @@ var upperCase = lowerCase.map(lower => lower.toUpperCase());
 
 // stores a random character in the variable 
 var randomUpperCaseLetter = randomUpperCase(upperCase);
-console.log(randomUpperCaseLetter);
+//console.log(randomUpperCaseLetter);
 
 var randomLowerCaseLetter = randomLowerCase(lowerCase);
-console.log(randomLowerCaseLetter);
+//console.log(randomLowerCaseLetter);
 
 var randomNumber = randomNumber(numbers);
-console.log(randomNumber);
+//console.log(randomNumber);
 
 var randomSymbol = randomSymbol(symbols);
-console.log(randomSymbol);
+//console.log(randomSymbol);
 
 
 
@@ -61,6 +65,121 @@ function randomSymbol (symbols){
 // console.log(randomSymbol(symbols));
 
 
+
+//prompt user for character count - store input into a global variable characterCount
+//prompt user to use lowercase
+    // if true add lowercase into empty array called randomArray
+        //if false continue
+//prompt user to use uppercase
+    // if true add uppercase array into randomArray
+        //if false continue
+//prompt user to use numbers
+    //if true add numbers array into randomArray
+        // if false continue
+//prompt user to use symbols
+    //if true add symbols array into randomArray
+        //if false continue
+
+// run character count loop set by the user input stored in variable characterCount
+  //each time the loop runs add one character pulled from the randomArray and store it in new variable newPassword
+  // need to generate random index using the mathfloor mathrandom and add that to the string each time the loop runs
+
+
+// if yes to lower add lowercase array to empty array
+    // if no then dont add array
+// if yes to upper add uppercase array to empty array
+// if yes to symbols add symbols array to empty array 
+
+// the new empty array will be the array the character count loop will pull characters from
+
+// run a character count for loop based on user input number
+
+// each time the loop runs add the result into an empty string value
+//that string value will be the generated password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generatePassword(){
+  var password = "";
+
+  var randomArray = []; // ask gary why this needs to be inside the function to work otherwise it gives concat error on first if statement
+
+  askUser()
+
+  function askUser(){
+    var characterCount = prompt("How many characters?");
+      if ( characterCount < 8 || characterCount > 128 ){
+  
+        alert("Please enter a number between 8 and 128")
+        askUser();
+      } 
+    
+      else {
+     
+      return characterCount
+       }   
+    
+    }
+  var includeLowerCase = confirm("Allow lower case characters?");
+     if (includeLowerCase){
+       var randomArray = randomArray.concat(lowerCase);
+       console.log(randomArray);
+     }       
+
+  var includeUpperCase = confirm("Allow upper case characters?");      
+    if (includeUpperCase){
+      var randomArray = randomArray.concat(upperCase);
+      console.log(randomArray);
+    }
+  var includeNumbers = confirm("Allow numbers?"); 
+    if (includeNumbers){
+      var randomArray = randomArray.concat(numbers);
+      console.log(randomArray);
+    }                     
+ 
+  var includeSpecialCharacters = confirm("Allow special characters?"); 
+    if (includeSpecialCharacters){
+      var randomArray = randomArray.concat(symbols);
+      console.log(randomArray);
+    }
+     
+    console.log(characterCount)
+    charCounter(characterCount);
+    
+
+
+
+    function charCounter(){
+      for (let i = 1; i <= characterCount; i++){
+        
+        var randomIndex = Math.floor(Math.random() * randomArray.length);
+      
+        //console.log(randomIndex);
+      
+        password += randomArray[randomIndex]
+      
+        console.log(password)
+      }
+      
+    }
+  return password
+   
+  
+}
+
+
+ //generatePassword();
 
 
 
